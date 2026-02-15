24f8c482a6d33e0804f17d9734a3a5f031a016cc
Marcos Leiva <estudioleiva@gmail.com>
2026-02-15 08:02:58 -0300

    agregando endpoint analizar

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class TextoInput(BaseModel):
+    texto: str
 
 @app.get("/")
 def root():
     return {"mensaje": "Servidor Python activo 🚀"}
+
+@app.post("/analizar")
+def analizar(data: TextoInput):
+    texto_recibido = data.texto
+    return {
+        "longitud_texto": len(texto_recibido),
+        "preview": texto_recibido[:50]
+    }
